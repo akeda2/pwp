83f586984c3938e262b6f359f452902c9f2341ab
akeda <akeda2@gmail.com>
2025-07-08 13:35:37 +0200

    fix: update file extension from .sh to .py
    - changed `pwp.sh` to `pwp.py`
    - updated script execution in `inst.sh`

--- inst.sh
+++ inst.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo install -m 755 pwp.sh /usr/local/bin/pwp && echo "pwp install SUCCESS!" || echo "pwp install FAIL!"
+sudo install -m 755 pwp.py /usr/local/bin/pwp && echo "pwp install SUCCESS!" || echo "pwp install FAIL!"
